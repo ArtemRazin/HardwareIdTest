10154f90ff81a5d074d5dbcbc7fac8accfce9c44
Artem Razin <razin@softanics.com>
2025-06-11 14:46:42 +0300

    Added initial files.

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿using ArmDot.Client;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine($"Hardware id: {Api.GetCurrentMachineHardwareId(Api.Hardware.CPU | Api.Hardware.HDD | Api.Hardware.Motherboard)}");
